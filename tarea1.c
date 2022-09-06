86691003b585a3535fd00254af46d402ea7959fb
Franco Seguel <francoseguellucero@gmail.com>
2022-09-06 17:41:56 -0300

    hipotesis y inicializacion de la matriz completa

---
+++ tarea1.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int **construirMatriz(int n){
+    int **miMatriz = malloc(sizeof(int*) * (n + 1));
+    for(int i = 0; i < n + 1; i++){
+        miMatriz[i] = malloc(sizeof(int) * (n + 1));
+    }
+
+    for (int i = 0; i < n + 1; i++){
+            miMatriz[0][i] = i;
+            miMatriz[i][0] = i;
+            printf("%i, %i, %i\n", i, 0, miMatriz[i][0]);
+            printf("%i, %i, %i\n", 0, i, miMatriz[0][i]);
+    }
+
+    return miMatriz;
+
+}
+
+void destruirMatriz(int **matriz, int n){
+    for (int i = 0; i < n + 1; i++){
+        free(matriz[i]);
+    }
+    free(matriz);
+}
+
+/*
+
+    Hipotesis 2.1: Omega(n) y 2n, => Theta(n)
+
+*/
+
+
+int main(int argc, char* argv[]){
+    int **miMatriz = construirMatriz(10);
+    destruirMatriz(miMatriz, 10);
+    return 0;
+}
