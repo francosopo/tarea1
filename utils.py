aabb389c02b653a3bbc16fb5d9af7731635af8be
Tobaltortugauwu <cristobal.suazo.ortiz@gmail.com>
2022-09-23 11:48:12 -0300

    a

--- utils.py
+++ utils.py
@@ -1,16 +1,16 @@
 import random
-
+#genera un archivo con caracteres de tamaño (size)*numPalabras
 def generarPalabra(size,numPalabras ):
     l = []
     abc = "abcdefghijklmnopqrstuvwxyz"
 
     for i in range(size):
         l.append(abc[random.randint(0,len(abc) - 1 )])
-    "".join(l)
+    l="".join(l)
 
     f = open(f'palabrasAleatorias_{size * numPalabras}.txt', 'a+')
-    for i in range(len(l)):
-        f.write(l[i])
+    
+    f.write(l)
 
     f.close()
 
@@ -23,13 +23,21 @@ def testPalabra(abc):
 def testLength():
     size= 30
     numPalabras = 100
-    generarPalabrass(size,numPalabras)
+    generarPalabra(size,numPalabras)
     nombreArchivo = f"palabrasAleatorias_{size*numPalabras}.txt"
-    with open()
+    contenido=""
+    with open(nombreArchivo,"r") as f:
+        contenido=f.read()
+    print(len(contenido))
+    print(size*numPalabras)
+    assert len(contenido)==size*numPalabras    
+
     
 def generarPalabrass(size, numPalabras):
     for i in range(numPalabras):
-        generarPalabra(size)
+        generarPalabra(size,numPalabras)
 
 #testPalabra("abcdefghijklmnopqrstuvwxyz")
-generarPalabrass(30,100)
+if(__name__=="__main__"):
+    generarPalabrass(30,100)
+    testLength()
