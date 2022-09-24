67142b23434fe5ac11078c74b560fc1847dc7a73
Simboraido <83623123+Simboraido@users.noreply.github.com>
2022-09-24 14:05:33 -0300

    algoritmo 2 alpha

--- algoritmo2.c
+++ algoritmo2.c
@@ -53,7 +53,11 @@ int comparador(int z, int y, int w){
     }
 }
 
-int minimo2()
+int calcular_W(int x, int y, int z, char c1, char c2){
+    int min_tentativo = min(y, z);
+    int minimo = minDiagonal(min_tentativo, x, c1, c2);
+    return minimo;
+}
 
 int algoritmo2(char *str1, char *str2){
     // diagonal, arriba, izq, valor
@@ -62,25 +66,51 @@ int algoritmo2(char *str1, char *str2){
         exit(1);
     }
     int len_str = strlen(str1);
-
-    int buff[4] = {0,0,0,0};
+    int buff[4] = {0,1,1,0};
     int posiciones[2] = {0,0}; //para saber si llego al borde
     int cuanto_falta_para_el_borde = len_str;
     int i = 0;
     int j = 0;
-    while(i < len_str && j < len_str){
-        int camino_a_seguir = comparador(buf[2], buf[1], buf[3]);
-        if(camino_a_seguir == 3) {
-            i++;j++;
-            buff[1] = buf[3];
-            
+    //Relleno la casilla faltante;
+    int minimo = calcular_W(buff[0],buff[1],buff[2],str1[i],str2[j])
+    buff[3] = minimo;
+    i++;
+    j++;
+    while(true){
+        if (i == len_str || j == len_str){
+            return buff[3]+ (len_str-i) + (len_str-j);
         }
-
-        int min_tentativo = min(buff[1], buff[2]);
-        int minimo = minDiagonal(min_tentativo, buff[0]);
-        buff[3] = minimo;
+        int camino_a_seguir = comparador(buf[2], buf[1], buf[3]);
+        else if(camino_a_seguir == 3) {
+            //calculo nuevo y
+            int new_y= calcular_W(buff[1],buff[1]+1,buff[3],str1[i],str2[j-1]);
+            buff[1]= new_y;
+            int new_z= calcular_W(buff[2], buff[3], buff[2]+1, str1[i-1], str2[j]);
+            buff[2]= new_z;
+            buff[1]=buff[3];
+            buff[3]= calcular_W(buff[0],buff[1],buff[2],str1[i],str2[j]);
+            i++;j++;
+        }
+        else if (camino_a_seguir==2){
+            int z=buff[2];
+            int w= buff[3];
+            buff[0]=z;
+            buff[1]=w;
+            buff[2]=z+1;
+            buff[3]=calcular_W(buff[0],buff[1], buff[2], str1[i],str2[j+1]);
+            j++;
+        }
+        else if(camino_a_seguir==1){
+            int y=buff[1];
+            int w=buff[3];
+            buff[0]=y;
+            buff[1]=y+1;
+            buff[2]=w;
+            buff[3]=calcular_W(buff[0],buff[1],buff[2],str1[i+1],str2[j]);
+            i++;
+        }
     }
-
+    return 0;
 }
 
 int obtenerValor(char *str1, char *str2){
