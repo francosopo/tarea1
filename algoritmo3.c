5e7c263da5bc51b6bead6ef2e51368ae7ac2c3ad
Franco Seguel <francoseguellucero@gmail.com>
2022-09-25 17:16:54 -0300

    avanzanado en el alg 3

--- algoritmo3.c
+++ algoritmo3.c
@@ -5,10 +5,33 @@
 
 // en el nombre del archivo va a decir si corresponde a una fila o a una columna
 // tamanho es el tamaño de una fila de la matriz
-int **matrizPequena(int tamanho, char *nombreArchivoFila, char *nombreArchivoColumna){
+
+/**
+ * 
+ * Crea una submatriz desde el archivo @param archivoFila y @param archivoColumna
+ * 
+ * @param tamanho es el tamaño de una fila de la matriz
+ * @param k es el indice desde donde se lee la fila o la columna solapada
+ * @param nombreArchivoFila es el nombre del archivo que contiene la fila indicada
+ * @param nombreArchivoColumna es el nombre del archivo que contiene la columna indicada
+*/
+int **matrizPequena(int tamanho,int k,int j, char *nombreArchivoFila, char *nombreArchivoColumna){
     int **matriz = calloc(tamanho,sizeof(int*));
-    int *fila = calloc(tamanho, sizeof(int));
-    int *columna =
+    for(int i = 0; i < tamanho; i++){
+        int *fila = calloc(tamanho, sizeof(int));
+        matriz[i] = fila;
+    }
+    obtenerDatos(matriz[0], tamanho,k,nombreArchivoFila);
+
+    int *columna = calloc(tamanho, sizeof(int));
+    obtenerDatos(columna, tamanho, j, nombreArchivoColumna);
+    
+    for(int i = 0; i < tamanho; i++){
+        matriz[0][i] = columna[i];
+    }
+
+    free(columna);
+
     return matriz;
 }
 
@@ -18,14 +41,14 @@ int **matrizPequena(int tamanho, char *nombreArchivoFila, char *nombreArchivoCol
  * Guarda los datos de @param fila en un archivo de nombre @param nombreArchivo
  * 
  * @param fila es el arreglo que se quiere guardar en  memoria externa
- * @param tamanhoDatos es el tamaño de una fila de la matriz
- * @param i es en donde inician los datos de esta matriz
- * @param j es en donde inician los datos de esta matriz
+ * @param tamanhoDatos es el tamaño de una fila de la submatriz
+ * @param k donde inician los datos de la fila o de la columna de la submatriz 
  * @param nombreArchivo es el nombre del archivo que tiene la fila o la columna
- * de la matriz
+ * solapada de la matriz
 */
-void guardarDatos(int *fila, int tamanhoDatos,int i, int j, char *nombreArchivo){
+void guardarDatos(int *fila, int tamanhoDatos,int k, char *nombreArchivo){
     FILE *in = fopen(nombreArchivo, "w");
+    fseek(in, sizeof(int) * tamanhoDatos * (k), SEEK_SET);
     for(int i =0; i < tamanhoDatos; i++){
         fwrite(&fila[i], sizeof(int), 1, in);
     }
@@ -38,11 +61,13 @@ void guardarDatos(int *fila, int tamanhoDatos,int i, int j, char *nombreArchivo)
  * Funcion que lee los datos del archivo @param nombreArchivo
  * 
  * @param fila donde guardar los datos
- * @param tamanhoDatos el tamaño de @param fila
+ * @param tamanhoDatos el tamaño de una fila de la submatriz
+ * @param k donde inicia la fila o la columna solapada de la submatriz 
  * @param nombreArchivo el nombre del archivo
 */
-void obtenerDatos(int *fila, int tamanhoDatos, char *nombreArchivo){
+void obtenerDatos(int *fila, int tamanhoDatos, int k, char *nombreArchivo){
     FILE *out = fopen(nombreArchivo, "r");
+    fseek(out, sizeof(int) * tamanhoDatos * k, SEEK_SET);
     for(int i = 0; i < tamanhoDatos; i++){
         fread(&fila[i], sizeof(int), 1, out);
     }
@@ -55,19 +80,35 @@ void obtenerDatos(int *fila, int tamanhoDatos, char *nombreArchivo){
  * 
  * Prueba que se guarde la informacion correctamente
 */
-void testguardar(){
+void testguardarDelPrincipio(){
     char *nombreArchivo= "miColumna.bin";
     int misDatos[9] = {1,2,3,4,5,6,7,8,9};
     int tamanho = 9;
-    guardarDatos(misDatos,tamanho,nombreArchivo);
+    guardarDatos(misDatos,tamanho,0,nombreArchivo);
     int deVuelta[9];
-    obtenerDatos(deVuelta, tamanho, nombreArchivo);
+    obtenerDatos(deVuelta, tamanho,0,nombreArchivo);
     for (int i = 0; i < tamanho; i++){
         if( misDatos[i] != deVuelta[i]){
-            printf("Error: expected %i, got %i", misDatos[i], deVuelta[i]);
+            fprintf(stderr,"Error: expected %i, got %i", misDatos[i], deVuelta[i]);
         }
     }
-    printf("Todo bien");
+    fprintf(stdout,"Todo bien\n");
+}
+
+void testGuardarEnMedio(){
+    char *nombreArchivo = "miColumna.bin";
+    int misDatos[9] = {1,2,3,4,5,6,7,8,9};
+    int tamanho = 9;
+    guardarDatos(misDatos, tamanho, 3, nombreArchivo);
+    int deVuelta[9];
+    obtenerDatos(deVuelta, tamanho, 3, nombreArchivo);
+    for(int i = 0; i < tamanho; i++){
+        if (misDatos[i] != deVuelta[i]){
+            fprintf(stderr,"Error, expected %i, got %i", misDatos[i], deVuelta[i]);
+        }
+    }
+    fprintf(stdout, "Todo bien\n");
+
 }
 
 
@@ -78,10 +119,14 @@ void testguardar(){
  * 
  * @param matriz la matriz
 */
-void destruirMatriz(int **matriz){
+void destruirMatriz(int **matriz, int tamanho){
+    for (int i = 0; i < tamanho; i++){
+        free(matriz[i]);
+    }
     free(matriz);
 }
 
 int main(int argc, char *argv[]){
-    testguardar();
+    testguardarDelPrincipio();
+    testGuardarEnMedio();
 }
