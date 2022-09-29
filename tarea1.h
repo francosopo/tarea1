c4eb9a1cf7e96e2bfafc596beacaf1b2b9e2d671
Franco Seguel <francoseguellucero@gmail.com>
2022-09-29 11:28:41 -0300

    tarea1.h

---
+++ tarea1.h
@@ -0,0 +1,15 @@
+
+/**
+ * Inicializa la matriz con tamaño @param n
+*/
+int **construirMatriz(int n);
+
+void destruirMatriz(int **matriz, int n);
+
+int min(int val1, int val2);
+
+int compararTamano( char * str1, char * str2);
+
+int obtenerValor(char *str1, char *str2);
+
+void completeMatrix(int **matrix, char *string1, char *string2);
