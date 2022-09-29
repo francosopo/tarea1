4ade9e1b929b49654dcdcfd5f02ff5877da82a66
Franco Seguel <francoseguellucero@gmail.com>
2022-09-28 21:36:23 -0300

    alg 2 completo y funcionanado

--- algoritmo2a.c
+++ algoritmo2a.c
@@ -16,6 +16,12 @@ int calcular_w(int *arr,int arr_len, char a, char b){
     int m = minimo(arr, arr_len);
     if( a == b) return m;
     else return m + 1;
+    /*if(m < arr[0]){
+        return m + 1;
+    }else{
+        if(a == b) return arr[0];
+        else return arr[0] + 1;
+    }*/
 }
 
 int obtenerValor2(char *str1, char *str2, int i, int j, int *buf){
@@ -41,26 +47,27 @@ int llenarArreglos(int *arr1, int *arr2, int arr_len, char *str1, char *str2){
     strcpy(str1_copy, str1);
     strcpy(str2_copy, str2);
 
-    for (int i = 0; i < arr_len - 1; i++){
+    for (int i = 0; i < arr_len; i++){
         //caso base
         if(i == 0){
             int buf[3] = {arr1[0], arr1[1], arr2[1]};
             arr1[i] = calcular_w(buf, 3, *str1_copy, *str2_copy);
             str1_copy++;
-            str2_copy++;
+            //str2_copy++;
         }else if(*str1 == '\0'){
             return arr1[0];
         }
         else{//caso iterativo
-            //printf("%i: %s, %s\n",i, str1_copy, str2_copy);
+            //printf("%s, %s\n", str1_copy, str2_copy);
             int buf[3] = {arr1[i - 1], arr1[i], arr1[i + 1]};
+        
             arr1[i] = calcular_w(buf, 3, *str1_copy, *str2_copy);
             str1_copy++;
-            str2_copy++;
+            //str2_copy++;
         }
-        printf("|%i|", arr1[i]);
+        //printf("|%i|", arr1[i]);
     }
-    printf("\n---------------------------------------------------\n");
+    //printf("\n---------------------------------------------------\n");
     free(str1_copy);
     free(str2_copy);
     return arr1[0];
@@ -75,25 +82,25 @@ int obtenerValorv2(char *str1, char *str2, int *valoresX, int *valoresY){
     }
 
     for(int i = 0; i < str_len; i++){
-        llenarArreglos(valoresX, valoresY, str_len - i, str1 + i, str2 + i);
+        llenarArreglos(valoresX, valoresY, str_len - i + 1, str1 + i, str2 + i);
         valoresY[0] = valoresX[0];
-        llenarArreglos(valoresY, valoresX, str_len - i, str1 + i, str2 + i);
+        //printf("Columna %i completa\n", i);
+        llenarArreglos(valoresY, valoresX, str_len - i + 1, str2 + i, str1 + i);
+        //printf("Fila %i completa\n", i);
         //printf("Aquii %i", i);
     }
     int res = valoresX[0];
-    printf("res: %i", res);
+    //printf("res: %i\n", res);
     return res;
 }
 
 
 int obtenerValorAlg2(char *str1, char *str2){
-	int str_len = strlen(str1);
+	int str_len = strlen(str1) + 1;
     //int buf[3];
     int *valoresX = calloc(str_len, sizeof(int));
     int *valoresY = calloc(str_len, sizeof(int));
     int res = obtenerValorv2(str1, str2, valoresX, valoresY);
-    
-
     free(valoresX); free(valoresY);
     return res;
 }
@@ -107,15 +114,17 @@ void TestComprobar(int expected, int got, int nTest){
 
 int main(int argc, char *argv[]){
     printf("Holi\n");
-    //int valor1 = obtenerValorAlg2("xxabcdefghijk", "abcdefghijkyy"); // deberia ser 4
+    int valor1 = obtenerValorAlg2("xxabcdefghijk", "abcdefghijkyy"); // deberia ser 4
     int valor2 = obtenerValorAlg2("banana", "ananas"); //deberia ser 2
     int valor3 = obtenerValorAlg2("francoso", "francoso"); // deberia ser 0
     int valor4 = obtenerValorAlg2("abcfdef","abcgdef"); // deberia ser 1
     int valor5 = obtenerValorAlg2("111222333", "111444333"); // deberia ser 3
+    int valor6 = obtenerValorAlg2("abcfdefghijk","abcdefghijkl"); // deberia ser 2
 
-    //TestComprobar(4, valor1, 1);
+    TestComprobar(4, valor1, 1);
     TestComprobar(2, valor2, 2);
     TestComprobar(0, valor3, 3);
     TestComprobar(1, valor4, 4);
     TestComprobar(3, valor5, 5);
+    TestComprobar(2, valor6, 6);
 }
