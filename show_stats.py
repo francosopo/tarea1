5a6cb0d088356a7fa3be620f66e764ca471df126
Franco Seguel <francoseguellucero@gmail.com>
2022-10-02 13:07:50 -0300

    Figuras mostradas en una sola imagen

--- show_stats.py
+++ show_stats.py
@@ -15,19 +15,19 @@ df_alg_1 = df[df['nalg'] == 1]
 df_alg_2 = df[df['nalg'] == 2]
 df_alg_3 = df[df['nalg'] == 3]
 
-fig, axes = plt.subplots(nrows=2, ncols=2, sharex=True, sharey=True)
+fig, axes = plt.subplots(nrows=1, ncols=3, sharex=True, sharey=True)
 
 
-df_alg_1.plot(ax=axes[0,0],x='tamano_string',y='tiempo_promedio_por_par',
+df_alg_1.plot(ax=axes[0],x='tamano_string',y='tiempo_promedio_por_par',
 xlabel='Cantidad de palabras', ylabel="Tiempo promedio [s]",
-title="Primer algoritmo", legend="Tiempo promedio por par")
+title="Programación Dinámica", legend="Tiempo promedio por par")
 
-df_alg_2.plot(ax=axes[0,1], x='tamano_string',y='tiempo_promedio_por_par',
+df_alg_2.plot(ax=axes[1], x='tamano_string',y='tiempo_promedio_por_par',
 xlabel='Cantidad de palabras', ylabel="Tiempo promedio [s]",
-title="Segundo algoritmo", legend="Tiempo promedio por par")
+title="Algoritmo en caché", legend="Tiempo promedio por par")
 
-df_alg_3.plot(ax=axes[1,0], x='tamano_string',y='tiempo_promedio_por_par',
+df_alg_3.plot(ax=axes[2], x='tamano_string',y='tiempo_promedio_por_par',
 xlabel='Cantidad de palabras', ylabel="Tiempo promedio [s]",
-title="Tercer algoritmo", legend="Tiempo promedio por par")
+title="Memoria secundaria", legend="Tiempo promedio por par")
 
 plt.show()
